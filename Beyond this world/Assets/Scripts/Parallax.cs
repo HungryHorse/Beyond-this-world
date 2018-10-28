63c3e74482cfbf96c189fff3b807fc75da3f7ddb
HungryHorse <33032163+HungryHorse@users.noreply.github.com>
2018-10-28 17:42:03 +0000

    temp background stuff
    parallax and standin bg

---
+++ Beyond this world/Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+    private Transform cameraPos;
+    private Transform target;
+    
+
+	// Use this for initialization
+	void Start () {
+        cameraPos = Camera.main.transform;
+        target.position = new Vector3(cameraPos.position.x, cameraPos.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.GetChild(0).position = target.position;
+		
+	}
+}
